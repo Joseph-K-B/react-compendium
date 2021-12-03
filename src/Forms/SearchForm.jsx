b3290abc9f1ad4cd484b53a48054a42aa0dd91f6
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-12-03 15:01:23 -0800

    green light on search form

--- src/Forms/SearchForm.jsx
+++ src/Forms/SearchForm.jsx
@@ -1,15 +1,16 @@
 
 function SearchForm({ name, handleSubmit, handleSearchChange }) {
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} aria-label='searchForm'>
             <h3>Search</h3>
             <label htmlFor='search'>Name:</label>
             <input 
             type='text'
+            aria-label='inputField'
             onChange={(e) => handleSearchChange(e.target.value)}
             value={name}
             />
-            <button >Submit</button>
+            <button aria-label='button'>Submit</button>
         </form>
     )
 }
