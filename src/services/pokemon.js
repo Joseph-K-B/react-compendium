dfc521d58a367a0008d62396aeb51766c42ab90b
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-12-05 12:39:30 -0800

    debating on a two column list with 10 per page, looks much better on mobile, could use pagination with only 10 per page, a custom hook to identify DPR and set grid accordingly would be worthwhile

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -2,7 +2,7 @@ import { pokemonMunger } from "../utils/helper";
 
 
 export const fetchPokemon = async () => {
-    const res = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex?&perPage=21`);
+    const res = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex?&perPage=10`);
     const pokemonData = await res.json();
     const pokemonMap = await pokemonData.results.map((item) => 
     pokemonMunger(item));
